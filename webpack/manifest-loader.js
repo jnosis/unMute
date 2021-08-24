73eaeac1b4704fb9f499e02dee15bebef99b8192
jnosis <jnosis1546@gmail.com>
2021-08-25 08:47:49 +0900

    Change options page to embedded in the prod build

--- webpack/manifest-loader.js
+++ webpack/manifest-loader.js
@@ -2,6 +2,7 @@ module.exports = function makeManifest(content) {
   console.log('manifest loaded');
   let manifest = JSON.parse(content);
   delete manifest.commands.dev;
+  delete manifest.options_ui.open_in_tab;
 
   return JSON.stringify(manifest);
 };
