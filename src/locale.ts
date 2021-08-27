c59024b51b09e410c169d8f511c817a01760d464
jnosis <jnosis1546@gmail.com>
2021-08-27 22:33:22 +0900

    Update TypeScript 4.3.5 to 4.4.2

--- src/locale.ts
+++ src/locale.ts
@@ -1,14 +1,16 @@
 export default function localizeHtmlPage() {
   const elements = document.querySelectorAll('.locale-text');
-  const messageRegex = /__MSG_(\w+)__/g;
+  const msgRegex = /__MSG_(\w+)__/g;
   for (const element of elements) {
-    const text = element.textContent as string;
-    const localizedText = text.replace(messageRegex, (_match, key: string) => {
-      return key ? chrome.i18n.getMessage(key) : text;
-    });
+    const text = element.textContent;
+    if (typeof text === 'string') {
+      const localizedText = text.replace(msgRegex, (_match, key: string) => {
+        return key ? chrome.i18n.getMessage(key) : text;
+      });
 
-    if (localizedText !== text) {
-      element.textContent = localizedText;
+      if (localizedText !== text) {
+        element.textContent = localizedText;
+      }
     }
   }
 }
